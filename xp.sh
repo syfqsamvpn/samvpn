c7bffcc462126886709b516178db72c91d1a14ed
syfqsamvpn <89333348+syfqsamvpn@users.noreply.github.com>
2021-10-26 12:18:12 +0800

    Add files via upload

--- xp.sh
+++ xp.sh
@@ -134,3 +134,17 @@ fi
 done
 systemctl restart v2ray@vless
 systemctl restart v2ray@vnone
+data=( `cat /etc/xray-mini/vless-direct.json | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/xray-mini/vless-direct.json" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray-mini/vless-direct.json
+fi
+done
+systemctl restart xray-mini@vless-direct
+systemctl restart xray-mini@vless-splice
